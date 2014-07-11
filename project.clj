46d838c929ae18ae97f9cc21e59be245f13b9ae4
Andrei Barbu <andrei@0xab.com>
2014-07-11 17:09:37 -0400

    0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com._0xab/common-clojure-platform "0.4.0"
+(defproject com._0xab/common-clojure-platform "0.6.0"
  :description "Misc Clojure code shared among multiple projects, these have platform-specific dependencies that aren't handled by lein"
  :url "https://github.com/abarbu/common-clojure-platform"
  :license {:name "Eclipse Public License"
@@ -15,7 +15,7 @@
                 [org.jblas/jblas "1.2.3"]
                 [com.taoensso/timbre "3.2.1"]
                 [com._0xab/clatrix "0.4.0-SNAPSHOT"]
-                [com._0xab/common-clojure "0.4.0"]
+                [com._0xab/common-clojure "0.6.0"]
                 ;; non-canonical fork because upstream hasn't been updated since 1.0
                 ;; but this is the original dev
                 [org.clojars.rosejn/clansi "1.2.0-SNAPSHOT"]])
